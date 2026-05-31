232f82b80888920d90a670e03038414253ad3c59
Josselin <robinjosselin5@gmail.com>
2026-05-31 11:32:20 +0200

    Correction of nav

--- modules/header.js
+++ modules/header.js
@@ -8,8 +8,8 @@ export function getHeader(basePath = ".") {
                 <li>
                     <a href="#">Theme 1</a>
                     <ul>
-                        <li id="1"><a href="#">Page 1</a></li>
-                        <li id="2"><a href="#">Page 2</a></li>
+                        <li id="1"><a href="/themes/template.html">Page 1</a></li>
+                        <li id="2"><a href="/themes/template.html">Page 2</a></li>
                         <li id="3"><a href="#">Page 3</a></li>
                     </ul>
                 </li>

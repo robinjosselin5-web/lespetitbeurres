e6773e5b3a614c9c7d04903d64d13982a1b887e4
czagoh <dorian.pennegat@hotmail.fr>
2026-06-04 18:13:19 +0200

    Test

--- modules/header.js
+++ modules/header.js
@@ -1,11 +1,13 @@
-
 export function getHeader(basePath = ".") {
         return `
         <nav>
-            <a class="logo" href="#"><img src="${basePath}/img/logo.svg" alt="LU logo"></a>
+            <a class="logo" href="./../biscuit.html"><img src="${basePath}/img/logo.svg" alt="LU logo"></a>
+            <button class="menu-toggle">
+                <i class="fa-solid fa-bars"></i>
+            </button>
             <ul class="menu">
 
-            </ul>
+             </ul> 
             <a class="btn-about" href="#">About</a>
         </nav>
         `;

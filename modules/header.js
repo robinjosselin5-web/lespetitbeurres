80401eba6adfa65419008ad013e52e9d9f4c1bec
Josselin <robinjosselin5@gmail.com>
2026-06-04 16:57:39 +0200

    link to deploye 25

--- modules/header.js
+++ modules/header.js
@@ -2,7 +2,7 @@
 export function getHeader(basePath = ".") {
         return `
         <nav>
-            <a class="logo" href="#"><img src="${basePath}/img/logo.svg" alt="LU logo"></a>
+            <a class="logo" href="./biscuit.html"><img src="${basePath}/img/logo.svg" alt="LU logo"></a>
             <button class="menu-toggle">
                 <i class="fa-solid fa-bars"></i>
             </button>

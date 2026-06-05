7df3df9b6c1651a3d43b037e6671ddc481aa47cd
czagoh <dorian.pennegat@hotmail.fr>
2026-06-05 11:45:47 +0200

    Fix chemin

--- modules/header.js
+++ modules/header.js
@@ -2,7 +2,7 @@
 export function getHeader(basePath = ".") {
         return `
         <nav>
-            <a class="logo" href="./biscuit.html"><img src="${basePath}/img/logo.svg" alt="LU logo"></a>
+            <a class="logo" href="${basePath}/biscuit.html"><img src="${basePath}/img/logo.svg" alt="LU logo"></a>
             <button class="menu-toggle">
                 <p>menu</p>
                 <i class="fa-solid fa-bars"></i>
